2895d2a519dfe1d6d60c4a5c23a0b1ecac0ceae2
Alina Kukhareva <akmrinn@gmail.com>
2017-09-28 16:11:13 +0700

    Move wrapping in tags to the separate function

--- js/reddit.js
+++ js/reddit.js
@@ -5,8 +5,8 @@ $(document).ready(function () {
     $("#btnRedditAccio").click(function(){
 
         $("#searchResults").html("<p>Loading... Please wait.</p>");
-        var searchTerm = $("#searchTerm").val();
-        var result = "";
+        let searchTerm = $("#searchTerm").val();
+        let result = "";
         $.ajax(
             "https://www.reddit.com/search.json",
             {
@@ -14,42 +14,14 @@ $(document).ready(function () {
 
                 success: function(responseData)
                 {
-
                     if (responseData.data.children.length > 0)
                     {
                         // how many results we have found
                         result += "<p># of results: " + responseData.data.children.length + "</p>";
 
-                        // we print results (each one is wrapped in '<p>' tag)
-                        $.each(responseData.data.children, function(idx, searchResult) {
-                            result += `<a href='${searchResult.data.url}'>
-                                        <div class='panel panel-default'>
-                                            <div class='panel-body'>
-                                                <div class='media'>
-                                                    <div class='media-left'>`;
-
-                            // if there's no thumbnail
-                            if (searchResult.data.thumbnail === "default")
-                            {
-	                            result += `<img src='${searchResult.data.preview.images[0].source.url}' 
-                                                class='media-object' style='width:60px'>`
-                            }
-                            else
-                            {
-	                            result += `<img src='${searchResult.data.thumbnail}' 
-                                                class='media-object' style='width:60px'>`;
-                            }
-
-                            result +=
-                                                    `</div>
-                                                    <div class='media-body'>
-                                                        ${searchResult.data.title}
-                                                    </div>
-                                                </div>
-	                                        </div>
-	                                    </div>
-                                    </a>`;
-                        });
+                        // we print results (each one is wrapped in 'panel' and 'media' tags)
+                        let wrappedItems = responseData.data.children.map(wrapElements);
+                        result += wrappedItems.join('');
                     }
                     else
                     {
@@ -63,13 +35,40 @@ $(document).ready(function () {
                     result = "Something didn't work!";
                     $("#searchResults").html(result);
                 }
-
             }
         );
-
     });
-
-
-
-
 });
+
+function wrapElements(element, index, array)
+{
+    // what tags we are wrapping elements in
+    let result = `<a href='${element.data.url}'>
+                    <div class='panel panel-default'>
+                        <div class='panel-body'>
+                            <div class='media'>
+                                <div class='media-left'>`;
+
+    // if there's no thumbnail we use this pic
+    if (element.data.thumbnail === "default")
+    {
+        result += `<img src='${element.data.preview.images[0].source.url}' 
+                        class='media-object' style='width:60px'>`
+    }
+    // else we use a thumbnail
+    else
+    {
+        result += `<img src='${element.data.thumbnail}' 
+                        class='media-object' style='width:60px'>`;
+    }
+
+    result += `</div>
+                                <div class='media-body'>
+                                    ${element.data.title}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>`;
+    return result;
+}
